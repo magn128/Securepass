ce6550b28d92d9dbab4822c7a37f938c85d2613f
magn128 <Magnus@wikerholmen.no>
2022-01-21 12:29:11 +0100

    jobbet aggresivt

--- securepass.py
+++ securepass.py
@@ -1,17 +1,20 @@
+from tkinter import CENTER
 import PySimpleGUI as sg
 from PIL import Image
 import io
 
-sg.ChangeLookAndFeel('darkblue16
-')
+sg.ChangeLookAndFeel('darkblue16')
 
 
 def win1_layout():
-    layout = [[ sg.Text('SecurePass'), sg.Text(size=(15, 1))]
-            ]
+    layout = [
+        [sg.Frame('',[[sg.Text('SecurePass', size=(120, 1), justification='center'),]])],
+        [sg.Frame('',[[sg.Text('media', size=(20,20))]])]
+    ]
     return layout
 
 window = sg.Window('SecurePass', win1_layout())
+
 
 while True:
     event, values = window.read()
