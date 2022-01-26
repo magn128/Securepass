9b952e8216ef408fc330560ab8ff5bf534940309
magn128 <Magnus@wikerholmen.no>
2022-01-26 10:23:29 +0100

    udated game

---
+++ main.py
@@ -0,0 +1,57 @@
+import PySimpleGUI as sg
+
+sg.theme('BluePurple')
+# Main - hovedside
+def main():
+    main = [
+        #row01
+        [sg.Frame('',[[sg.Text('Securepass', justification='center', size=(84, 1), font=(15))]])],
+        #row02
+        [sg.Frame('',[[sg.Text('Categories', justification='center', size=(20, 20))]]),
+        sg.Frame('',[[sg.Text('Login info', size=(96, 20))]])
+        ]
+    ]
+    return main
+# Start - startside
+def start():
+    start = [
+        #row01
+        [sg.Text('Database name', size=(15, 1)), sg.InputText('')],
+        #row02
+        [sg.Text('Password', size=(15, 1)), sg.InputText('', key='Password', password_char='*')],
+        #row03
+        [sg.Button('Login'),
+        sg.Button('Create database')]
+    ]
+    return start
+
+def create():
+    create = [
+        #row01
+        [sg.Text('Name:', size=(15, 1)), sg.InputText('')],
+        #row02
+        [sg.Text('Password', size=(15, 1)), sg.InputText('', key='Password', password_char='*')],
+        #row03
+        [sg.Button('Create database')]
+            ]
+    return create
+
+def add():
+    add = [
+        #row01
+        [sg.Text('Add Categories', size=(15, 1))],
+        #row02
+        [sg.Text('Name:', size=(15, 1)), sg.InputText('')],
+        #row03 
+        [sg.Button('Add')]
+        ]
+    return add
+
+window = sg.Window('SecurePass', main())()
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event in (None, 'exit'):
+        break
+window.close()
