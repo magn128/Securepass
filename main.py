c4d7cf55a89944425a578ad6f2e1e73c990e4939
magn128 <Magnus@wikerholmen.no>
2022-01-26 14:55:34 +0100

    made buttons function
    made buttons open different windows

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from pdb import run
 import PySimpleGUI as sg
+
+
+
+def test_func():
+    global create
+    create = 'veryglobal'
+    print (create)
 
 sg.theme('BluePurple')
 # Main - hovedside
@@ -16,7 +24,7 @@ def main():
 def start():
     start = [
         #row01
-        [sg.Text('Database name', size=(15, 1)), sg.InputText('')],
+        [sg.Text('Database name', size=(15, 1)), sg.InputText('', key='Database_name')],
         #row02
         [sg.Text('Password', size=(15, 1)), sg.InputText('', key='Password', password_char='*')],
         #row03
@@ -37,7 +45,9 @@ def create():
         #row03
         [sg.Button('Create database')]
             ]
+    print(create)
     return create
+
 
 
 # Add - add ny kategori
@@ -54,22 +64,62 @@ def add():
     return add
 
 
+username = 'magne'
+password = '123'
+
 window = sg.Window('SecurePass', start())
 
-
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event in (None, 'exit'):
-        break
-    if event == 'Create database':
-        window.close()
-        window2 = sg.Window('SecurePass', create())
-        while True:
-            event, values = window.read()
-            print(event, values)
-            if event in (None, 'exit'):
+def login_func():
+    global window, window2, window3, username, password
+    run_create_win = False
+    run_main_win = False
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event in ('Login'):
+            if values['Database_name'] == username and values['Password'] == password:
+                window.close()
+                window3 = sg.Window('SecurePass', main())
+                run_main_win = True
                 break
+
+        if run_main_win:
+            main_func()
+
+        
+        if event == 'Create database':
+            window.close()
+            window2 = sg.Window('SecurePass', create())
+            run_create_win = True
+            break
+    
+    if run_create_win:
+        create_func()
+
+def create_func():
+    global window2
+    while True:
+        event2, values2 = window2.read()
+        print(event2, values2)
+        if event2 in (None, 'exit'):
+            break
+
+
+def main_func():
+    global window3
+    while True:
+        event3, values3 = window3.read()
+        if event3 in (None, 'exit'):
+            break
+
+
+
+
+
+
+login_func()
+main_func()
 
 
 window.close()
+
